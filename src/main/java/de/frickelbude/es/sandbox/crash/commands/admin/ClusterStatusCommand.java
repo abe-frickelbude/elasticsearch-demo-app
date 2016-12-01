c040a816e5b75daac832d1368f88160cad8d146c
abe <ibragim.kuliev@googlemail.com>
2016-12-02 00:06:33 +0100

    - disabled CraSH integration for now

--- src/main/java/de/frickelbude/es/sandbox/crash/commands/admin/ClusterStatusCommand.java
+++ src/main/java/de/frickelbude/es/sandbox/crash/commands/admin/ClusterStatusCommand.java
@@ -1,22 +1,26 @@
 package de.frickelbude.es.sandbox.crash.commands.admin;
 
-import org.crsh.cli.*;
-import org.crsh.command.*;
 import org.elasticsearch.action.admin.cluster.health.*;
 import org.elasticsearch.client.*;
 import org.elasticsearch.cluster.health.*;
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.stereotype.*;
 
-@Component
-public class ClusterStatusCommand extends BaseCommand {
+/**
+ * This doesn't work yet!
+ * 
+ * @author Ibragim Kuliev
+ *
+ */
+// @Component
+public class ClusterStatusCommand /* extends BaseCommand */ {
 
     @Autowired
     private Client client;
 
-    @Command
-    @Named("cluster-health")
-    @Usage("Show current ES cluster health status")
+    //
+    // @Command
+    // @Named("cluster-health")
+    // @Usage("Show current ES cluster health status")
     public ClusterHealthStatus getClusterStatus() {
         AdminClient adminClient = client.admin();
         ClusterHealthResponse response = adminClient.cluster().prepareHealth().execute().actionGet();
